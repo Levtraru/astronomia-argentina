93607cc6e2770627e4ae4d62fa1abd4ba2bab298
Levtraru <lautarogt94@gmail.com>
2022-10-12 09:30:14 -0300

    animacion fade-in news

--- js/main.js
+++ js/main.js
@@ -2,20 +2,21 @@ const btnHam = document.querySelector(".ham-btn");
 const btnTimes = document.querySelector(".times-btn");
 const navBar = document.querySelector(".nav-links-750px");
 
+/* ======== Navbar ======== */
+/* Controla el comportamiento de la barra de navegacion de manera responsiva */
 // Se llama al cambiar el tamaño de la ventana
 window.addEventListener("resize", function () {
   var viewport_width = window.innerWidth;
 
-  /* Esconde el hamburger menu si el ancho de pantalla es mayor
-   a 750  */
+  // Esconde el hamburger menu si el ancho de pantalla es mayor
+  // a 750
   if (viewport_width > 750) {
     btnHam.style.display = "none";
     btnTimes.style.display = "none";
     navBar.classList.remove("show-nav");
   }
   // Si es menor a 750, muestra btnHam a menos que btnTimes y la navbar ya sean visible
-  else if (btnTimes.style.display != "block")
-  {
+  else if (btnTimes.style.display != "block") {
     btnHam.style.display = "block";
   }
 });
@@ -37,3 +38,18 @@ btnTimes.addEventListener("click", function () {
     navBar.classList.remove("show-nav");
   }
 });
+/* ======== Navbar End ======== */
+
+/* Hace que las noticias aparezcan con un fade al ser observadas por el usuario */
+// Aplica o remueve el estilo 'shown' cuando un elemento es observado
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("shown");
+    } else {
+      entry.target.classList.remove("shown");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
